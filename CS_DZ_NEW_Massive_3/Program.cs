3b2e938f1d18b1e07109ef0c947cd2f781c37dd8
Rasl174 <98830529+Rasl174@users.noreply.github.com>
2022-03-14 10:21:01 +0500

    Добавьте файлы проекта.

---
+++ CS_DZ_NEW_Massive_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_DZ_NEW_Massive_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Что я здесь делаю? Мама забери меня обратно!";
+            string[] separatedText = text.Split(' ');
+
+            foreach (var word in separatedText)
+            {
+                Console.WriteLine(word);
+            }
+        }
+    }
+}
